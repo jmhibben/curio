97b7313452b4bc2809be38ebcdd74fbaf7529385
James Hibben <jmhibben@gmail.com>
2019-07-19 13:14:15 -0500

    Put lines used for testing in their own file

---
+++ packages/jexle/test/helpers/static/lines.ts
@@ -0,0 +1,11 @@
+export const testLines = [
+    '# A happy little file',
+    'I only have a few lines, but that\'s okay. I enjoy having sparse contents.',
+    '## I have a few headings, though',
+    '### Headings are fun, you see',
+    '## But they\'re easy to abuse, too',
+    '### And sometimes',
+    '#### Just sometimes',
+    '##### You might go',
+    '###### A little too deep'
+  ]
